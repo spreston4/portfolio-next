d6f989badb7f06080d808119acc8611b21aeb751
Sam Preston <sam.preston@headway.io>
2023-10-30 21:00:39 -0700

    contact component - form inop

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,26 +1,58 @@
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import * as Icon from "react-feather";
 
 const Contact = () => {
   return (
-    <div className="container bg-pink">
-      <div className="flex flex-row items-center justify-between gap-x-6">
-        <div className="text-dark w-1/2">
-          <h2 className="heading-small">Get in Touch</h2>
-          <p className="body-medium">
-            Have a question or a project in mind? I&apos;d love to hear from
-            you. Let&apos;s chat and make something happen together.
-          </p>
+    <div className="container bg-pink relative">
+      <div className="flex flex-row justify-between gap-x-6">
+        <div className="text-dark w-1/2 h-full flex flex-col gap-y-20">
+          <div>
+            <h2 className="heading-small mb-6">Get in Touch</h2>
+            <p className="body-medium">
+              Have a question or a project in mind? I&apos;d love to hear from
+              you. Let&apos;s chat and make something happen together.
+            </p>
+          </div>
+          <div className="flex flex-col items-start justify-center gap-y-6 body-medium">
+            <div className="icon-info">
+              <Icon.PhoneCall />
+              <p>+1 (503) 956-4531</p>
+            </div>
+            <div className="icon-info">
+              <Icon.Mail />
+              <p>sam.preston11@gmail.com</p>
+            </div>
+            <div className="icon-info">
+              <Icon.MapPin />
+              <p>Hillsboro, OR</p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-x-8">
+            <a
+              href="https://www.linkedin.com/in/sam-preston-5601b311a/"
+              target="_blank"
+            >
+              <div className="icon-bg-white">
+                <Icon.Linkedin className="icon-small" fill="text-dark" />
+              </div>
+            </a>
+            <a href="https://github.com/spreston4" target="_blank">
+              <div className="icon-bg-white">
+                <Icon.GitHub className="icon-small" fill="text-dark" />
+              </div>
+            </a>
+          </div>
         </div>
-        <form className="w-2/5 mt-4 flex flex-col items-start justify-between gap-y-6">
+        <form className="w-2/5 mt-4 flex flex-col gap-y-6">
           <Input placeholder="Name" />
           <Input placeholder="Email" />
           <Input placeholder="Phone Number" />
-          <Input placeholder="Message" type="textarea"/>
+          <Input placeholder="Message" type="textarea" />
           <Button>Send Message</Button>
-
         </form>
       </div>
+      <div className="h-[200px] w-[45px] absolute left-0 top-[40%] bg-red rounded-tr-full rounded-br-full"></div>
     </div>
   );
 };
