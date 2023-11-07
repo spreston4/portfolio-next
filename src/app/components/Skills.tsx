ae75e2500300dd5cb76d8f358394259c3eb88416
Sam Preston <sam.preston@headway.io>
2023-11-07 12:53:34 -0800

    infinite scroll added to skills component

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -23,14 +23,29 @@ const Skills = () => {
   ];
   return (
     <div
-      className="container bg-white text-dark flex flex-col items-center"
+      className="container bg-white text-dark flex flex-col items-center overflow-hidden"
       id="skills"
     >
       <h2 className="heading-small mb-6">Core Skills</h2>
-      <div className="flex flex-row items-center justify-evenly gap-x-16 gap-y-6 flex-wrap">
-        {skillBank.map(({ skill, image }) => (
-          <SkillCard skill={skill} src={image} key={skill} />
-        ))}
+      <div
+        x-data="{}"
+        className="mt-8 flex flex-row flex-nowrap gap-x-24 w-full overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]"
+      >
+        <ul
+          x-ref="logos"
+          x-init="$nextTick(() => {
+            let ul = $refs.logos;
+            ul.insertAdjacentHTML('afterend', ul.outerHTML);
+            ul.nextSibling.setAttribute('aria-hidden', 'true');
+           })"
+          className="flex flex-row items-center justify-center gap-x-24 flex-nowrap animate-infinite-scroll [&_img]:max-w-none"
+        >
+          {skillBank.map(({ skill, image }) => (
+            <li key={skill}>
+              <SkillCard skill={skill} src={image} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
