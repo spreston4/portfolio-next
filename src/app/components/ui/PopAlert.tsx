52fd0e25ac9c4ccb27076e4f873d19ea3544fa71
Sam Preston <sam.preston@headway.io>
2023-11-07 13:36:03 -0800

    PopAlert rework

--- src/app/components/ui/PopAlert.tsx
+++ src/app/components/ui/PopAlert.tsx
@@ -1,5 +1,4 @@
-import Button from "./Button";
-import { Check } from "react-feather";
+import { Check, X } from "react-feather";
 
 interface PopAlertProps {
   message: string;
@@ -12,13 +11,18 @@ const PopAlert = ({ message, onClose, timeout }: PopAlertProps) => {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen flex items-end justify-end z-50">
       <div
-        className={`bg-white p-6 body-medium flex items-center justify-center gap-6 shadow-lg ${slide}`}
+        className={`bg-white py-4 pl-4 pr-10 body-medium flex items-center justify-start gap-2 shadow-xl relative ${slide}`}
       >
-        <div className="icon-bg-large bg-success">
+        <div className="h-[40px] w-[40px] rounded-full flex items-center justify-center bg-success">
           <Check className="text-white" />
         </div>
         <p>{message}</p>
-        <Button onClick={onClose}>Close</Button>
+        <div
+          className="icon-bg-small text-dark-grey absolute top-2 right-2"
+          onClick={onClose}
+        >
+          <X className="h-[16px]" />
+        </div>
       </div>
     </div>
   );
