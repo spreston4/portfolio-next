6eccc0bbcb5b4a23c8b7f4b74337fe5dbb29d2ac
Sam Preston <sam.preston@headway.io>
2023-11-07 08:08:53 -0800

    next config for gh deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  generateStaticParams: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
