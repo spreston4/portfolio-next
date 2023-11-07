196dd650bd6d97e8a423f9396e9ba92c61d5f891
Sam Preston <sam.preston@headway.io>
2023-11-07 13:01:23 -0800

    solved ts error with alpine

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  generateStaticParams: function () {
-    return {
-      "/": { page: "/" },
-    };
-  },
 };
 
 module.exports = nextConfig;
